20e57f68cd3ff0f0ea52b2b575c805f3e28ec44f
John Olugbemi <jjohnayo17@gmail.com>
2025-04-14 10:39:43 +0100

    internet checker added

--- comovie/lib/infrastructure/remote_services/remote_data_source.dart
+++ comovie/lib/infrastructure/remote_services/remote_data_source.dart
@@ -2,19 +2,27 @@ import 'dart:convert';
 import 'package:comovie/infrastructure/remote_services/http_service/i_http_services.dart';
 import 'package:comovie/infrastructure/remote_services/i_remote_data_source.dart';
 import 'package:comovie/domain/get_movies/objects/get_movies_response_object.dart';
+import 'package:comovie/infrastructure/remote_services/internet_checker/internet_info.dart';
 import 'package:comovie/presentation/core/api_execptions.dart';
+import 'package:comovie/presentation/core/execptions.dart';
 import 'package:comovie/presentation/core/utils/constants.dart';
+import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
 
 @LazySingleton(as: IRemoteDataSource)
 class RemoteDataSource implements IRemoteDataSource {
   final IHttpServices _iHttpServices;
+  final INetworkInfo _networkInfo;
 
-  RemoteDataSource(this._iHttpServices);
+  RemoteDataSource(this._iHttpServices, this._networkInfo);
 
   @override
   Future<List<MovieModel>> getAllMovies() async {
-    final res = await _iHttpServices.get(ApiConstants.getMovies);
+
+     if (!await _networkInfo.isConnected) {
+       throw NoInternetConnectionException();
+    } else{
+       final res = await _iHttpServices.get(ApiConstants.getMovies);
     if (res.statusCode == 200) {
       final List results = res.data['results'];
       return results.map((json) => mapToMovie(json)).toList();
@@ -22,6 +30,8 @@ class RemoteDataSource implements IRemoteDataSource {
       final errorMessage = res.data['status_message'] ?? 'Failed to load movies';
        throw ApiException(errorMessage, statusCode: res.statusCode);
     }
+    }
+    
   }
 }
 
